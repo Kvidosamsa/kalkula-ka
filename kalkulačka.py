85864f7bd37a1a9647c1dad8258fc462cf3ceeeb
Kvidosamsa <159123624+Kvidosamsa@users.noreply.github.com>
2024-04-03 10:19:41 +0200

    Add files via upload

---
+++ kalkulačka.py
@@ -0,0 +1,31 @@
+print("zde jsi v kalkulačce \n pro ukončení programu napiš 'konec' ")
+
+while True:
+ cislo1 = float (input("první číslo :"   ))
+ cislo2 = float (input("druhé číslo :"   ))
+
+ operace = input ("jakou chceš operaci ? '+, -, *, /, **, odmocneni, konec    :         ")
+ if operace == "+" :
+  print (cislo1 + cislo2)
+ elif operace == "-" :
+  print (cislo1 - cislo2)
+ elif operace == "*" :
+  print (cislo1 * cislo2)
+ elif operace == "/" :
+  print (cislo1 / cislo2)
+ elif operace =="**" :
+  print (cislo1 ** cislo2)
+ elif operace == "odmocneni":
+  print (cislo1 **(1/cislo2))
+ elif operace == "konec" :
+  print ("kalkulačka se ukončuje")
+  break
+ else:
+  print("napiš správnou operaci")
+
+break 
+
+
+
+
+
